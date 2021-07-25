611feded5772f2f42fea096de0897043138852f6
Ridgeway <ridge@wrld.tech>
2021-07-25 15:36:23 -0500

    initiallizing bundaru special 42

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          WRLD.vc is in Development
+          WRLD.ltd is in Development
         </p>
         <a
           className="App-link"
