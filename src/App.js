ed97ad867387f1b0da8dd8c0cf11248ea9aa0db9
Ridgeway <ridge@wrld.tech>
2021-07-25 14:52:13 -0500

    initializing
    Oh, yeah

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './wrld.svg';
 import './App.css';
 
 function App() {
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          WRLD.vc is in Development
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://wrld.tech/contact"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Get Involved / Learn More
         </a>
       </header>
     </div>
